a4df630cff53a60aae60a25725d9c46477a01a17
Brahimkharmachi <bkharmachi@yahoo.fr>
2022-03-15 16:24:04 +0100

    checkpoint jsx

---
+++ src/App.js
@@ -0,0 +1,21 @@
+import './App.css';
+import  Image  from  './imageInSrc.jpg.jpg'
+
+function App() {
+  return (
+    <>
+  <div style={{ border: "solid 1px black", maxWidth: "100vw" }}>
+    <h1 className="title-red">Your name here</h1>
+    <br />
+    <img src={Image}/>
+    <br />
+    <img src="/imageInPublic.jpg.jpg" />
+  </div>
+  <video controls="" width={320} height={240}>
+    <source src="myVideo.mp4" type="video/mp4" />
+  </video>
+</>
+  );
+}
+
+export default App;
